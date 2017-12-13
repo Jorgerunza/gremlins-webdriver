3e6beffa716572ac3b9fb5dc5569f1520366017b
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2017-12-13 18:03:47 +0000

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -119,7 +119,12 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-    reporters: ['spec'],
+    reporters: ['spec', 'junit'],
+	reporterOptions: {
+		junit: {
+			outputDir: './'
+		}
+	},
     
     //
     // Options to be passed to Jasmine.
