148a7f99b5af4db57d7cc42e8ce73be20952522b
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2017-12-14 03:12:57 -0500

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,7 +8,7 @@ exports.config = {
     // from which `wdio` was called. Notice that, if you are calling `wdio` from an
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
-    //
+    port: 4444,
     specs: [
         './test/specs/**/*.js'
     ],
